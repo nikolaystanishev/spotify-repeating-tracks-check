46a71a8ce9d8eee3e58de5c085ebc2063ccc5fe6
nikolaystanishev <stanishev.nikolay@gmail.com>
2019-03-09 14:23:35 +0200

    Add example setup of env

---
+++ set-env.sh
@@ -0,0 +1,5 @@
+conda activate spotify-repeating-tracks-check
+
+export SPOTIPY_CLIENT_ID='your-spotify-client-id'
+export SPOTIPY_CLIENT_SECRET='your-spotify-client-secret'
+export SPOTIPY_REDIRECT_URI='your-app-redirect-url'
